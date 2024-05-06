837d1581e63908cca6050e6fe931970b6dad8f04
Farid Andika <faridandika159@gmail.com>
2024-05-06 11:20:41 +0700

    Initial Commit

---
+++ MorningTestGit/MorningTestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorningTestGitApp.swift
+//  MorningTestGit
+//
+//  Created by Farid Andika on 06/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MorningTestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
